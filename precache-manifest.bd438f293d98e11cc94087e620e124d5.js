b2a90363743bd7fa2300ceec35778b650b5b6757
Thiago <tsuna2221@live.com>
2018-11-22 01:18:55 -0200

    Updates

---
+++ precache-manifest.bd438f293d98e11cc94087e620e124d5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe56f6da7d7ffe65fc888392e6cae6f9",
+    "url": "/piccitr/static/media/SubmitSearch.fe56f6da.svg"
+  },
+  {
+    "revision": "15c7ebf3e5e5993cfb821f16b50e9a60",
+    "url": "/piccitr/static/media/SettingsButton.15c7ebf3.svg"
+  },
+  {
+    "revision": "80979d47e439e131574342ddf52e6997",
+    "url": "/piccitr/static/media/CloseButton.80979d47.svg"
+  },
+  {
+    "revision": "9e006c7a80fb7063c739c913292654a1",
+    "url": "/piccitr/static/media/Arrow.9e006c7a.svg"
+  },
+  {
+    "revision": "86341425cf2eb38912ca",
+    "url": "/piccitr/static/js/runtime~main.86341425.js"
+  },
+  {
+    "revision": "0c84ea793406c7c5966f",
+    "url": "/piccitr/static/js/main.0c84ea79.chunk.js"
+  },
+  {
+    "revision": "1416af0253d6f90e5f62",
+    "url": "/piccitr/static/js/1.1416af02.chunk.js"
+  },
+  {
+    "revision": "0c84ea793406c7c5966f",
+    "url": "/piccitr/static/css/main.64de2410.chunk.css"
+  },
+  {
+    "revision": "d16bf8d9061e2cfd465900c210807b3f",
+    "url": "/piccitr/index.html"
+  }
+];
